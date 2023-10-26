e780538452b3268e4d1444b9a310fcf8e3f77e00
kashifkhan099 <139098480+kashifkhan099@users.noreply.github.com>
2023-10-26 12:10:46 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,72 @@
+const classNo = 19;
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+
+// var result = sum(2, 4, 6);
+// var result1 = sum(2, 466, 6);
+// var result2 = sum(2, 44, 6);
+
+// console.log(result);
+// console.log(result1);
+// console.log(result2);
+
+// function sum() {
+//   var b = 12;
+//   var a = 10;
+// }
+// console.log(b);
+
+// //? call/print before declaration.
+// sum();
+// console.log(a);
+
+// var a = 10;
+
+// function sum() {
+//   a = 12;
+// }
+
+// console.log(a);
+// sum();
+
+// let a = 10;
+
+// console.log(a);
+
+// a = 12;
+
+// a = 16;
+// console.log(a);
+
+// sum();
+// function sum() {
+//   console.log(12);
+// }
+// alert(a);
+// let a;
+
+// console.log(a * a);
+
+// var day = "march";
+// if (day === "jan") {
+//   console.log("January");
+// } else if (day === "feb") {
+//   console.log("Feburary");
+// } else if()
+var day = "fri";
+
+switch (day) {
+    // //? let day = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+    case "mon":
+    console.log("Monday");
+    // break;
+  case "tue":
+    console.log("Tuesday");
+    // break;
+  case "sun":
+    console.log("Sunday");
+    // break;
+  default:
+    alert("No day Match!");
+}
